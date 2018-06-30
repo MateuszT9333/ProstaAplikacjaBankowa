53cc3c888315bf801845716c70b09814e2ea7035
Mateusz <t.mateusz9333@gmail.com>
2018-06-30 16:29:59 +0200

    frontend

--- src/main/java/pl/mateusz/springdemo/controllers/FrontController.java
+++ src/main/java/pl/mateusz/springdemo/controllers/FrontController.java
@@ -1,8 +1,14 @@
 package pl.mateusz.springdemo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
 
+    @GetMapping("/")
+    public String greeting() {
+
+        return "main";
+    }
 }
